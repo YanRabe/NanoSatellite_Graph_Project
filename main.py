26e3ef941b4d21ab577454ae290051c99ed72358
yra7512 <yan.rabefaniraka@etu.inp-n7.fr>
2026-01-29 17:16:10 +0100

    fix: commented graph save lines

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def _format_axes(ax):
 #     viz.plot(ESSAIMS[id][:, 0], ESSAIMS[id][:, 1], ESSAIMS[id][:, 2], 'ro')
 #     plt.title(f"Représentation de l'essaim de {id}")
 
-# # plt.show()
+plt.show()
 
 # affichage des essaims avec les arêtes
 for e_id, (name, coord) in enumerate(ESSAIMS.items()):
@@ -100,146 +100,151 @@ for e_id, (name, coord) in enumerate(ESSAIMS.items()):
         # plt.title(f"Représentation de l'essaim de {name} avec la configuration {p_id}")
         # fig.tight_layout()
 
-# plt.show()
+plt.show()
 
 ### analyse pour les graphes non valués (Partie 2)
-# for e_id, (name, coord) in enumerate(ESSAIMS.items()):
-#     for p_id, portee in enumerate(PORTEES):
-#         essaim = essaims_G[e_id][p_id]
+for e_id, (name, coord) in enumerate(ESSAIMS.items()):
+    for p_id, portee in enumerate(PORTEES):
+        essaim = essaims_G[e_id][p_id]
         
-#         print(f"\n[ Analyse Pour la config : {name} - {portee / 1000}km ]")
+        print(f"\n[ Analyse Pour la config : {name} - {portee / 1000}km ]")
 
-#         #DEGRÉ MOYEN
-#         deg_moyen = 0
-#         for i in essaim.nodes:
-#             deg_moyen += essaim.degree(i)
+        #DEGRÉ MOYEN
+        deg_moyen = 0
+        for i in essaim.nodes:
+            deg_moyen += essaim.degree(i)
 
-#         deg_moyen = deg_moyen / essaim.number_of_nodes()
-#         DEGREE_MOYEN = deg_moyen
-#         print("Degré moyen :", DEGREE_MOYEN)
+        deg_moyen = deg_moyen / essaim.number_of_nodes()
+        DEGREE_MOYEN = deg_moyen
+        print("Degré moyen :", DEGREE_MOYEN)
 
-#         # DISTRIB DU DEG
-#         essaim_degree = np.zeros((1))
-#         for i in essaim.nodes:
-#             essaim_degree = np.append(essaim_degree,essaim.degree(i))
+        # DISTRIB DU DEG
+        essaim_degree = np.zeros((1))
+        for i in essaim.nodes:
+            essaim_degree = np.append(essaim_degree,essaim.degree(i))
 
-#         res = plt.hist(essaim_degree)
-#         plt.xlabel("satellites")
-#         plt.ylabel("degré")
-#         plt.title(f"Distribution du degré par satellite ({name} - {portee / 1000}km)")
-#         plt.savefig(f"img/{name}_{portee / 1000}_deg_distsrib.png".replace(" ", "_"),
-#                 dpi=300,
-#                 bbox_inches='tight') #sauvegarde le graphe en png
-#         # plt.show()
-
-
-#         # CLUSTERING MOYEN
-#         clustering_moyen = nx.average_clustering(essaim)
-#         CLUSTERING_MOYEN = clustering_moyen
-#         print("Moyernne du clustering :", clustering_moyen)
+        res = plt.hist(essaim_degree)
+        plt.xlabel("satellites")
+        plt.ylabel("degré")
+        plt.title(f"Distribution du degré par satellite ({name} - {portee / 1000}km)")
+        #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+        # plt.savefig(f"img/{name}_{portee / 1000}_deg_distsrib.png".replace(" ", "_"),
+                # dpi=300,
+                # bbox_inches='tight') #sauvegarde le graphe en png
+        plt.show()
 
 
-#         # DISTRIBUTION DU DEGRE DE CLUSTERING
-#         clustering_nodes = nx.clustering(essaim)
-#         clustering_values = np.array(list(clustering_nodes.values()))
-#         plt.figure()
-#         plt.hist(clustering_values, bins=20)
-#         plt.xlabel("Degré de clustering")
-#         plt.ylabel("Nombre de satellites")
-#         plt.title(f"Distribution du degré de clustering ({name} - {portee / 1000}km)")
-#         plt.savefig(f"img/{name}_{portee / 1000}_clustering_distrib.png".replace(" ", "_"),
-#                 dpi=300,
-#                 bbox_inches='tight') #sauvegarde le graphe en png
-#         # plt.show()
+        # CLUSTERING MOYEN
+        clustering_moyen = nx.average_clustering(essaim)
+        CLUSTERING_MOYEN = clustering_moyen
+        print("Moyernne du clustering :", clustering_moyen)
 
 
-#         #CLIQUE | on utilise fiind_cliques qui ne récupère que les cliques maximales
-#         # parce que sinon la complexité explose et ca n'en finit pas de tourner...
-#         clique_by_node = list(nx.find_cliques(essaim))
-#         NOMBRE_CLIQUE = len(clique_by_node)
-#         print("Nombre de cliques (maximales) : " +str(NOMBRE_CLIQUE))
+        # DISTRIBUTION DU DEGRE DE CLUSTERING
+        clustering_nodes = nx.clustering(essaim)
+        clustering_values = np.array(list(clustering_nodes.values()))
+        plt.figure()
+        plt.hist(clustering_values, bins=20)
+        plt.xlabel("Degré de clustering")
+        plt.ylabel("Nombre de satellites")
+        plt.title(f"Distribution du degré de clustering ({name} - {portee / 1000}km)")
+        #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+        # plt.savefig(f"img/{name}_{portee / 1000}_clustering_distrib.png".replace(" ", "_"),
+        #         dpi=300,
+        #         bbox_inches='tight') #sauvegarde le graphe en png
+        plt.show()
 
 
-#         # ORDRE DES CLIQUES
-#         taille_cliques = [len(c) for c in clique_by_node]
-#         if taille_cliques:
-#             plt.figure()
-#             plt.hist(taille_cliques)
-#             plt.xlabel("Taille de la clique")
-#             plt.ylabel("Nombre de cliques")
-#             plt.title(f"Distribution de l'ordre des cliques ({name} - {portee / 1000}km)")
-#             plt.savefig(f"img/{name}_{portee / 1000}_cliques_ordre_distrib.png".replace(" ", "_"),
-#                 dpi=300,
-#                 bbox_inches='tight') #sauvegarde le graphe en png
-#             # plt.show()
+        #CLIQUE | on utilise fiind_cliques qui ne récupère que les cliques maximales
+        # parce que sinon la complexité explose et ca n'en finit pas de tourner...
+        clique_by_node = list(nx.find_cliques(essaim))
+        NOMBRE_CLIQUE = len(clique_by_node)
+        print("Nombre de cliques (maximales) : " +str(NOMBRE_CLIQUE))
 
 
-#         #COMPOSANTES CONNEXES
-#         NOMBRE_CONNEXE = nx.number_connected_components(essaim)
-#         print("Nombre de composantes connexes : " + str(NOMBRE_CONNEXE))
+        # ORDRE DES CLIQUES
+        taille_cliques = [len(c) for c in clique_by_node]
+        if taille_cliques:
+            plt.figure()
+            plt.hist(taille_cliques)
+            plt.xlabel("Taille de la clique")
+            plt.ylabel("Nombre de cliques")
+            plt.title(f"Distribution de l'ordre des cliques ({name} - {portee / 1000}km)")
+            #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+            # plt.savefig(f"img/{name}_{portee / 1000}_cliques_ordre_distrib.png".replace(" ", "_"),
+            #     dpi=300,
+            #     bbox_inches='tight') #sauvegarde le graphe en png
+            plt.show()
 
 
-#         #ORDRE DES COMPOSANTES CONNEXES
-#         compo_connexes = list(nx.connected_components(essaim))
-#         taille_connexes = [len(c) for c in compo_connexes]
-#         print(f"Ordre des composantes connexes : {taille_connexes}")
-#         if taille_connexes:
-#             plt.figure()
-#             plt.hist(taille_connexes)
-#             plt.xlabel("Taille (ordre) de la composante")
-#             plt.ylabel("Nombre de composantes")
-#             plt.title(f"Distribution de l'ordre des composantes connexes ({name} - {portee / 1000}km)")
-#             plt.savefig(f"img/{name}_{portee / 1000}_connexes_ordre_distrib.png".replace(" ", "_"),
-#                 dpi=300,
-#                 bbox_inches='tight') #sauvegarde le graphe en png
-#             # plt.show()
+        #COMPOSANTES CONNEXES
+        NOMBRE_CONNEXE = nx.number_connected_components(essaim)
+        print("Nombre de composantes connexes : " + str(NOMBRE_CONNEXE))
 
-#         #PLUS COURT CHEMINS ET LONGUEUR
-#         mat_court_chemin = np.empty((NB_SAT,NB_SAT),dtype=object)
-#         mat_longueur_chemin = np.zeros((NB_SAT,NB_SAT))
+
+        #ORDRE DES COMPOSANTES CONNEXES
+        compo_connexes = list(nx.connected_components(essaim))
+        taille_connexes = [len(c) for c in compo_connexes]
+        print(f"Ordre des composantes connexes : {taille_connexes}")
+        if taille_connexes:
+            plt.figure()
+            plt.hist(taille_connexes)
+            plt.xlabel("Taille (ordre) de la composante")
+            plt.ylabel("Nombre de composantes")
+            plt.title(f"Distribution de l'ordre des composantes connexes ({name} - {portee / 1000}km)")
+            #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+            # plt.savefig(f"img/{name}_{portee / 1000}_connexes_ordre_distrib.png".replace(" ", "_"),
+            #     dpi=300,
+            #     bbox_inches='tight') #sauvegarde le graphe en png
+            plt.show()
+
+        #PLUS COURT CHEMINS ET LONGUEUR
+        mat_court_chemin = np.empty((NB_SAT,NB_SAT),dtype=object)
+        mat_longueur_chemin = np.zeros((NB_SAT,NB_SAT))
         
-#         all_paths = dict(nx.all_pairs_shortest_path(essaim))
+        all_paths = dict(nx.all_pairs_shortest_path(essaim))
 
-#         for i in range(NB_SAT):
-#             for j in range(NB_SAT):
-#                 if i in all_paths and j in all_paths[i]:
-#                     # Chemin trouvé
-#                     path = all_paths[i][j]
-#                     mat_court_chemin[i][j] = path
+        for i in range(NB_SAT):
+            for j in range(NB_SAT):
+                if i in all_paths and j in all_paths[i]:
+                    # Chemin trouvé
+                    path = all_paths[i][j]
+                    mat_court_chemin[i][j] = path
                     
-#                     # Calcul de la distance
-#                     d = 0.0
-#                     for k in range(1, len(path)):
-#                         u, v = path[k-1], path[k]
-#                         d += dist_matrix[u][v]
-#                     mat_longueur_chemin[i][j] = d
-#                 else:
-#                     # Pas de chemin
-#                     mat_court_chemin[i][j] = None
+                    # Calcul de la distance
+                    d = 0.0
+                    for k in range(1, len(path)):
+                        u, v = path[k-1], path[k]
+                        d += dist_matrix[u][v]
+                    mat_longueur_chemin[i][j] = d
+                else:
+                    # Pas de chemin
+                    mat_court_chemin[i][j] = None
 
 
-#         # DISTRIB PLUS COURTS CHEMINS
-#         path_lengths = []
-#         for i in range(NB_SAT):
-#             for j in range(i + 1, NB_SAT):
-#                 if mat_court_chemin[i][j] is not None:
-#                     path_lengths.append(mat_longueur_chemin[i][j])
+        # DISTRIB PLUS COURTS CHEMINS
+        path_lengths = []
+        for i in range(NB_SAT):
+            for j in range(i + 1, NB_SAT):
+                if mat_court_chemin[i][j] is not None:
+                    path_lengths.append(mat_longueur_chemin[i][j])
         
-#         if path_lengths:
-#             plt.figure()
-#             plt.hist(path_lengths, bins=20)
-#             plt.xlabel("Longueur du chemin (distance)")
-#             plt.ylabel("Fréquence")
-#             plt.title(f"Distribution des plus courts chemins ({name} - {portee / 1000}km)")
-#             plt.savefig(f"img/{name}_{portee / 1000}_shortest_distrib.png".replace(" ", "_"),
-#                 dpi=300,
-#                 bbox_inches='tight') #sauvegarde le graphe en png
-#             # plt.show()
+        if path_lengths:
+            plt.figure()
+            plt.hist(path_lengths, bins=20)
+            plt.xlabel("Longueur du chemin (distance)")
+            plt.ylabel("Fréquence")
+            plt.title(f"Distribution des plus courts chemins ({name} - {portee / 1000}km)")
+            #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+            # plt.savefig(f"img/{name}_{portee / 1000}_shortest_distrib.png".replace(" ", "_"),
+            #     dpi=300,
+            #     bbox_inches='tight') #sauvegarde le graphe en png
+            plt.show()
 
 
-#         # Nombre des plus courts chemins
-#         nb_chemins = len(path_lengths)
-#         print(f"Nombre de plus courts chemins: {nb_chemins}\n")
+        # Nombre des plus courts chemins
+        nb_chemins = len(path_lengths)
+        print(f"Nombre de plus courts chemins: {nb_chemins}\n")
 
 
 ### Analayse pour les graphes valués (Partie 3)
@@ -276,10 +281,11 @@ for e_id, (name, coord) in enumerate(ESSAIMS.items()):
     plt.xlabel("satellites")
     plt.ylabel("degré")
     plt.title(f"Distribution du degré par satellite ({name} - {portee / 1000}km - Valué)")
-    plt.savefig(f"img/{name}_{portee / 1000}_deg_distsrib_VALUED.png".replace(" ", "_"),
-                dpi=300,
-                bbox_inches='tight') #sauvegarde le graphe en png
-    # plt.show()
+    #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+    # plt.savefig(f"img/{name}_{portee / 1000}_deg_distsrib_VALUED.png".replace(" ", "_"),
+    #             dpi=300,
+    #             bbox_inches='tight') #sauvegarde le graphe en png
+    plt.show()
 
 
     # CLUSTERING MOYEN
@@ -296,10 +302,11 @@ for e_id, (name, coord) in enumerate(ESSAIMS.items()):
     plt.xlabel("Degré de clustering")
     plt.ylabel("Nombre de satellites")
     plt.title(f"Distribution du degré de clustering ({name} - {portee / 1000}km - Valué)")
-    plt.savefig(f"img/{name}_{portee / 1000}_clustering_distrib_VALUED.png".replace(" ", "_"),
-                dpi=300,
-                bbox_inches='tight') #sauvegarde le graphe en png
-    # plt.show()
+    #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+    # plt.savefig(f"img/{name}_{portee / 1000}_clustering_distrib_VALUED.png".replace(" ", "_"),
+    #             dpi=300,
+    #             bbox_inches='tight') #sauvegarde le graphe en png
+    plt.show()
 
 
     #CLIQUE
@@ -316,10 +323,11 @@ for e_id, (name, coord) in enumerate(ESSAIMS.items()):
         plt.xlabel("Taille de la clique")
         plt.ylabel("Nombre de cliques")
         plt.title(f"Distribution de l'ordre des cliques ({name} - {portee / 1000}km - Valué)")
-        plt.savefig(f"img/{name}_{portee / 1000}_cliques_ordre_distrib_VALUED.png".replace(" ", "_"),
-                dpi=300,
-                bbox_inches='tight') #sauvegarde le graphe en png
-        # plt.show()
+        #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+        # plt.savefig(f"img/{name}_{portee / 1000}_cliques_ordre_distrib_VALUED.png".replace(" ", "_"),
+        #         dpi=300,
+        #         bbox_inches='tight') #sauvegarde le graphe en png
+        plt.show()
 
 
     #COMPOSANTES CONNEXES
@@ -338,10 +346,11 @@ for e_id, (name, coord) in enumerate(ESSAIMS.items()):
         plt.xlabel("Taille (ordre) de la composante")
         plt.ylabel("Nombre de composantes")
         plt.title(f"Distribution de l'ordre des composantes connexes ({name} - {portee / 1000}km - Valué)")
-        plt.savefig(f"img/{name}_{portee / 1000}_connexes_ordre_distrib_VALUED.png".replace(" ", "_"),
-                dpi=300,
-                bbox_inches='tight') #sauvegarde le graphe en png
-        # plt.show()
+        #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+        # plt.savefig(f"img/{name}_{portee / 1000}_connexes_ordre_distrib_VALUED.png".replace(" ", "_"),
+        #         dpi=300,
+        #         bbox_inches='tight') #sauvegarde le graphe en png
+        plt.show()
 
 
     #PLUS COURT CHEMINS ET LONGUEUR
@@ -383,10 +392,11 @@ for e_id, (name, coord) in enumerate(ESSAIMS.items()):
         plt.xlabel("Coût du chemin (somme des distances au carré)")
         plt.ylabel("Fréquence")
         plt.title(f"Distribution des plus courts chemins (valués) ({name} - {portee / 1000}km)")
-        plt.savefig(f"img/{name}_{portee / 1000}_shortest_distsrib_VALUED.png".replace(" ", "_"),
-                dpi=300,
-                bbox_inches='tight') #sauvegarde le graphe en png
-        # plt.show()
+        #DÉCOMMENTER POUR ENREGISTRER LE GRAPHE
+        # plt.savefig(f"img/{name}_{portee / 1000}_shortest_distsrib_VALUED.png".replace(" ", "_"),
+        #         dpi=300,
+        #         bbox_inches='tight') #sauvegarde le graphe en png
+        plt.show()
 
 
     # Nombre des plus courts chemins
